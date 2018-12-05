f1afe5b6013784362166c9c2cefef4fbae4af83e
zyp <whbsmyp@163.com>
2018-12-05 21:23:48 +0800

    修改配置

--- testweb/src/main/java/com/yya/controller/DataController.java
+++ testweb/src/main/java/com/yya/controller/DataController.java
@@ -22,7 +22,7 @@ public class DataController {
         String code = resultSet.getCode();
         Student stu = resultSet.getT();
         String nameBack = stu.getName();
-        logger.info("+++++++++返回码是："+code+",name:"+nameBack);
+        logger.info("+++++++++返回码是aa："+code+",name:"+nameBack);
         return "success";
     }
 }
